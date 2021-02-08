1939093fa2819e8390bc8fdb00b6f1694d9cb9f3
Niels-Frederik Brinck <niels.frederik.brinck@gmail.com>
2021-02-08 11:24:22 +0100

    Added skeleton for expressJS api. Added the same endpoints as found in minitwit.py, however, they do not have any functionality yet. Also added gitignore for nodejs

---
+++ expressjs/index.js
@@ -0,0 +1,57 @@
+import Express from "express";
+
+const app = Express();
+const port = 5000;
+
+//Shows a users timeline or if no user is logged in it will
+//redirect to the public timeline.  This timeline shows the user's
+//messages as well as all the messages of followed users
+app.get('/', (req, res) => {
+    res.sendStatus(200)
+  })
+  
+//Displays the latest messages of all users
+app.get('/public', (req,res) =>
+{
+    res.sendStatus(200)
+})
+
+//Displays a users tweets
+app.get('/:id', (res,req) =>
+{
+    res.sendStatus(200)
+})
+
+app.post('/{username}/follow', (req,res) =>
+{
+    res.sendStatus(200)
+})
+
+app.delete('/:username/unfollow', (req,res) =>
+{
+    res.sendStatus(200)
+})
+
+app.post('/add_message', (req,res) =>
+{
+    res.sendStatus(200)
+})
+
+app.get('/login', (req,res) =>
+{
+    res.sendStatus(200)
+})
+
+app.post('/register', (req,res) =>
+{
+    res.sendStatus(200)
+})
+
+app.delete('/logout', (req,res) =>
+{
+    res.sendStatus(200)
+})
+
+app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`)
+})
