9b9cb9347f204d092b16cf06afd3f83c8d371289
Hampus Michael Wadum Iversen <haiv@itu.dk>
2021-02-26 17:27:59 +0100

    added simulator api

--- docker_start_script.sh
+++ docker_start_script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 sudo docker stop api
 sudo docker stop frontend
+sudo docker stop api-sim
 sudo docker rm api
 sudo docker rm frontend
+sudo docker rm api-sim
 sudo docker run -d -p 5000:5000 --name api nieb/api
 sudo docker run -d -p 3000:80 --name frontend nieb/frontend
