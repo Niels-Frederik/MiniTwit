8e367a9296a159b03ffd0d5d065bc3060c3af9f2
marq <marq@itu.dk>
2021-03-15 10:20:11 +0100

    removed console log

--- API/index.js
+++ API/index.js
@@ -24,9 +24,6 @@ app.get('/', async (req, res) => {
     else res.redirect("timeline");
 })
 
-console.log(dotenv);
-
-
 app.get('/timeline', async(req,res) =>
 {
     const userId = await getUserIdFromJwtToken(req);
