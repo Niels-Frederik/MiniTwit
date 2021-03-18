29647ae7865da1bf635932beea22f61e3d4e7695
Yarlermanden <mmho@itu.dk>
2021-03-18 11:16:13 +0100

    refactored monitoring

--- API/monitoring.js
+++ API/monitoring.js
@@ -7,202 +7,51 @@ class Monitoring {
 	console.log('printing' + register)
 	prom.collectDefaultMetrics({ register })
 
-	this.request_counter = new prom.Counter({
-	  name: 'minitwit_total_requests',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.request_counter)
+	this.request_counter = createMetric('minitwit_total_requests', register, prom)
+	this.information_response_counter = createMetric('minitwit_information_response_counter', register, prom)
+	this.success_response_counter = createMetric('minitwit_successful_response_counter', register, prom)
+	this.redirect_response_counter = createMetric('minitwit_redirect_response_counter', register, prom)
+	this.client_error_response_counter = createMetric('minitwit_client_error_response_counter', register, prom)
+	this.server_error_response_counter = createMetric('minitwit_server_error_response_counter', register, prom)
 
-	this.information_response_counter = new prom.Counter({
-	  name: 'minitwit_information_response_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.information_response_counter)
-
-	this.success_response_counter = new prom.Counter({
-	  name: 'minitwit_successful_response_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.success_response_counter)
-
-	this.redirect_response_counter = new prom.Counter({
-	  name: 'minitwit_redirect_response_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.redirect_response_counter)
-
-	this.client_error_response_counter = new prom.Counter({
-	  name: 'minitwit_client_error_response_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.client_error_response_counter)
-
-	this.server_error_response_counter = new prom.Counter({
-	  name: 'minitwit_server_error_response_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.server_error_response_counter)
-
-
-	this.follow_request_counter = new prom.Counter({
-	  name: 'minitwit_follow_request_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.follow_request_counter)
-
-	this.follow_success_counter = new prom.Counter({
-	  name: 'minitwit_follow_success_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.follow_success_counter)
-
-	this.follow_failure_counter = new prom.Counter({
-	  name: 'minitwit_follow_failure_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.follow_failure_counter)
-
-
-	this.unfollow_request_counter = new prom.Counter({
-	  name: 'minitwit_unfollow_request_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.unfollow_request_counter)
-
-	this.unfollow_success_counter = new prom.Counter({
-	  name: 'minitwit_unfollow_success_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.unfollow_success_counter)
-
-	this.unfollow_failure_counter = new prom.Counter({
-	  name: 'minitwit_unfollow_failure_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.unfollow_failure_counter)
-
-
-	this.getFollows_request_counter = new prom.Counter({
-	  name: 'minitwit_getFollows_request_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.getFollows_request_counter)
-
-	this.getFollows_success_counter = new prom.Counter({
-	  name: 'minitwit_getFollows_success_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.getFollows_success_counter)
-
-	this.getFollows_failure_counter = new prom.Counter({
-	  name: 'minitwit_getFollows_failure_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.getFollows_failure_counter)
-
-
-	this.register_request_counter = new prom.Counter({
-	  name: 'minitwit_register_request_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.register_request_counter)
-
-	this.register_success_counter = new prom.Counter({
-	  name: 'minitwit_register_success_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.register_success_counter)
-
-	this.register_failure_counter = new prom.Counter({
-	  name: 'minitwit_register_failure_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.register_failure_counter)
-
-
-	this.getMessages_request_counter = new prom.Counter({
-	  name: 'minitwit_getMessages_request_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.getMessages_request_counter)
-
-	this.getMessages_success_counter = new prom.Counter({
-	  name: 'minitwit_getMessages_success_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.getMessages_success_counter)
-
-	this.getMessages_failure_counter = new prom.Counter({
-	  name: 'minitwit_getMessages_failure_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.getMessages_failure_counter)
+	this.follow_request_counter = createMetric('minitwit_follow_request_counter', register, prom)
+	this.follow_success_counter = createMetric('minitwit_follow_success_counter', register, prom)
+	this.follow_failure_counter = createMetric('minitwit_follow_failure_counter', register, prom)
 	
+	this.unfollow_request_counter = createMetric('minitwit_unfollow_request_counter', register, prom)
+	this.unfollow_success_counter = createMetric('minitwit_unfollow_success_counter', register, prom)
+	this.unfollow_failure_counter = createMetric('minitwit_unfollow_failure_counter', register, prom)
 
-	this.getMessages_username_request_counter = new prom.Counter({
-	  name: 'minitwit_getMessages_username_request_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.getMessages_username_request_counter)
+	this.getFollows_request_counter = createMetric('minitwit_getFollows_request_counter', register, prom)
+	this.getFollows_success_counter = createMetric('minitwit_getFollows_success_counter', register, prom)
+	this.getFollows_failure_counter = createMetric('minitwit_getFollows_failure_counter', register, prom)
 
-	this.getMessages_username_success_counter = new prom.Counter({
-	  name: 'minitwit_getMessages_username_success_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.getMessages_username_success_counter)
-	
-	this.getMessages_username_failure_counter = new prom.Counter({
-	  name: 'minitwit_getMessages_username_failure_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.getMessages_username_failure_counter)
+	this.register_request_counter = createMetric('minitwit_register_request_counter', register, prom)
+	this.register_success_counter = createMetric('minitwit_register_success_counter', register, prom)
+	this.register_failure_counter = createMetric('minitwit_register_failure_counter', register, prom)
 
+	this.getMessages_request_counter = createMetric('minitwit_getMessages_request_counter', register, prom)
+	this.getMessages_success_counter = createMetric('minitwit_getMessages_success_counter', register, prom)
+	this.getMessages_failure_counter = createMetric('minitwit_getMessages_failure_counter', register, prom)
 
-	this.messages_request_counter = new prom.Counter({
-	  name: 'minitwit_messages_request_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.messages_request_counter)
-	
-	this.messages_success_counter = new prom.Counter({
-	  name: 'minitwit_messages_success_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.messages_success_counter)
+	this.getMessages_username_request_counter = createMetric('minitwit_getMessages_username_request_counter', register, prom)
+	this.getMessages_username_success_counter = createMetric('minitwit_getMessages_username_success_counter', register, prom)
+	this.getMessages_username_failure_counter = createMetric('minitwit_getMessages_username_failure_counter', register, prom)
 
-	this.messages_failure_counter = new prom.Counter({
-	  name: 'minitwit_messages_failure_counter',
-	  help: 'metric_help',
-	  registers: [register],
-	});
-	register.registerMetric(this.messages_failure_counter)
+	this.messages_request_counter = createMetric('minintwit_messages_request_counter', register, prom)
+	this.messages_success_counter = createMetric('minintwit_messages_success_counter', register, prom)
+	this.messages_failure_counter = createMetric('minintwit_messages_failure_counter', register, prom)
   }
+}
+
+function createMetric(metricName, register, prom) {
+  const metric = new prom.Counter({
+	name: metricName,
+	help: 'metric_help',
+	registers: [register]
+  });
+  register.registerMetric(metric)
+  return metric;
 }
 
 module.exports.Monitoring = Monitoring
